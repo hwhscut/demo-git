05e8df0a34f6743d0b48781fd123b08a19993bfa
hwhscut <hwh-scut@qq.com>
2021-11-27 22:36:50 +0800

    The first time commit to git.

---
+++ src/test/java/psn/hwh/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package psn.hwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
