5a8282fd1d78d76c6136e5fdd512a9a536d7168d
hwhscut <hwh-scut@qq.com>
2021-11-28 11:43:19 +0800

    暂时屏蔽数据源

--- src/main/java/psn/hwh/DemoGitApplication.java
+++ src/main/java/psn/hwh/DemoGitApplication.java
@@ -1,9 +1,12 @@
 package psn.hwh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class DemoGitApplication {
 
 	public static void main(String[] args) {
